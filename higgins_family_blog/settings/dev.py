9603778518a2e50df25682d114b4d203b45680c3
Gene Higgins <genotype@gknow.com>
2022-06-22 12:54:55 -0600

    first commit

---
+++ higgins_family_blog/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-wg_sbk5yc(2eypia$ytmwl9cf45nlu6#h$51=7=-fue^o^s%@+'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
